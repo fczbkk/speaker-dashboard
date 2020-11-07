dbdc5efb42709ca131490f343b84a7b63dbd156c
Riki Fridrich <riki@fczbkk.com>
2020-11-07 16:31:43 +0100

    feat: add ProgressBar component

--- app/src/components/ProgressBar.vue
+++ app/src/components/ProgressBar.vue
@@ -1,13 +1,36 @@
 <template>
-$END$
+  <div class="progressBar">
+    <div class="progressIndicator" :style="indicatorStyle">&nbsp;</div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "ProgressBar"
+name: "ProgressBar",
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    indicatorStyle () {
+      return {
+        width: `${100 * this.value}%`
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import '../variables';
 
+  .progressBar {
+    // border: 1px solid red;
+  }
+  .progressIndicator {
+    background: $colorMedium;
+    line-height: 1rem;
+  }
 </style>
